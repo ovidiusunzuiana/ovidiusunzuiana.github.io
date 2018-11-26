4b9b29b5763fda9d0ba65dfbf53e717b71843f63
ovidiusunzuiana <ovidiu.sunzuiana@yahoo.com>
2018-11-26 12:13:27 +0200

    line width and cirle update ( 2 stroke)

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.3";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.5";
 
  var canvas=document.getElementById("id_canvas");
  canvas.addEventListener("touchstart", on_touch_start);
@@ -32,17 +32,22 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
 	   for( var i=0; i< e.changedTouches.length; i++){
 		   var context=canvas.getContext("2d");
 		   context.beginPath();
+		   context.lineWidth=20;
 		   context.moveTo(last_pos.x- canvas_bounding_rect.left, last_pos.y- canvas_bounding_rect.top);
 		   context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 		   e.changedTouches[i].pageY - canvas_bounding_rect.left.top);
-		   last_pos.x=e.changedTouches[i].pageX;
-		   last_pos.y=e.changedTouches[i].pageY;
+		   context.stroke();
+		   context.beginPath();
+		   context.lineWidth=1;
+		   
 		   context.arc(e.changedTouches[i].pageX- canvas_bounding_rect.left,
 		    e.changedTouches[i].pageY - canvas_bounding_rect.top,
 			10,
 			0,
 			2* Math.PI);
 		 context.stroke();	
-
+		 last_pos.x=e.changedTouches[i].pageX;
+		last_pos.y=e.changedTouches[i].pageY;
+	   }
 	   
 	   }
