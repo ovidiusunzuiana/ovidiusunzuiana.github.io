a217898bbb554fdaf5d83b0558636ea5ca9835a4
ovidiusunzuiana <ovidiu.sunzuiana@yahoo.com>
2018-11-26 11:44:13 +0200

    draw line touch

--- touch.js
+++ touch.js
@@ -1,8 +1,12 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.0";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.3";
 
  var canvas=document.getElementById("id_canvas");
  canvas.addEventListener("touchstart", on_touch_start);
- 
+ canvas.addEventListener("touchmove", on_touch_move);
+  
+  var canvas_bounding_rect=canvas.getBoundingClientRect();
+  
+   var last_pos={x: 0, y: 0};
  
    function on_touch_start(e)
    {
@@ -10,8 +14,31 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
 	   for( var i=0; i< e.changedTouches.length; i++){
 		   var context=canvas.getContext("2d");
 		   context.beginPath();
-		   context.arc(e.changedTouches[i].pageX,
-		    e.changedTouches[i].pageY,
+		   context.arc(e.changedTouches[i].pageX- canvas_bounding_rect.left,
+		    e.changedTouches[i].pageY - canvas_bounding_rect.top,
+			10,
+			0,
+			2* Math.PI);
+		 context.stroke();	
+		last_pos.x=e.changedTouches[i].pageX;
+		last_pos.y=e.changedTouches[i].pageY;
+	   
+	   }
+   }
+   
+    function on_touch_move(e)
+   {
+		e.preventDefault();
+	   for( var i=0; i< e.changedTouches.length; i++){
+		   var context=canvas.getContext("2d");
+		   context.beginPath();
+		   context.moveTo(last_pos.x- canvas_bounding_rect.left, last_pos.y- canvas_bounding_rect.top);
+		   context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
+		   e.changedTouches[i].pageY - canvas_bounding_rect.left.top);
+		   last_pos.x=e.changedTouches[i].pageX;
+		   last_pos.y=e.changedTouches[i].pageY;
+		   context.arc(e.changedTouches[i].pageX- canvas_bounding_rect.left,
+		    e.changedTouches[i].pageY - canvas_bounding_rect.top,
 			10,
 			0,
 			2* Math.PI);
@@ -19,4 +46,3 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
 
 	   
 	   }
-   }
