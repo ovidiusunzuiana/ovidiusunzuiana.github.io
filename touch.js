f249a2a8f53878d843cce743e94a2c56f07e0f82
ovidiusunzuiana <ovidiu.sunzuiana@yahoo.com>
2018-11-26 12:39:30 +0200

    2 fingers draw

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.5";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.6";
 
  var canvas=document.getElementById("id_canvas");
  canvas.addEventListener("touchstart", on_touch_start);
@@ -6,7 +6,10 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
   
   var canvas_bounding_rect=canvas.getBoundingClientRect();
   
-   var last_pos={x: 0, y: 0};
+  
+   
+   var pozitii=[];
+   
  
    function on_touch_start(e)
    {
@@ -20,9 +23,14 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
 			0,
 			2* Math.PI);
 		 context.stroke();	
-		last_pos.x=e.changedTouches[i].pageX;
-		last_pos.y=e.changedTouches[i].pageY;
-	   
+		  var last_pos={x: e.changedTouches[i].pageX , y: e.changedTouches[i].pageY, id: e.changedTouches[i].identifier };
+		 
+		//last_pos.x=e.changedTouches[i].pageX;
+		//last_pos.y=e.changedTouches[i].pageY;
+		//last_pos.id=e.changedTouches[i].identifier;
+		pozitii.push(last_pos);
+
+		
 	   }
    }
    
@@ -30,10 +38,14 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
    {
 		e.preventDefault();
 	   for( var i=0; i< e.changedTouches.length; i++){
+		   var j=0;
+		   for(; j<pozitii.length; j++)
+			if( pozitii[j].id==e.changedTouches[i].identifier)  
+		     break;
 		   var context=canvas.getContext("2d");
 		   context.beginPath();
 		   context.lineWidth=20;
-		   context.moveTo(last_pos.x- canvas_bounding_rect.left, last_pos.y- canvas_bounding_rect.top);
+		   context.moveTo(pozitii[j].x - canvas_bounding_rect.left, pozitii[j].y- canvas_bounding_rect.top);
 		   context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 		   e.changedTouches[i].pageY - canvas_bounding_rect.left.top);
 		   context.stroke();
@@ -46,8 +58,8 @@ document.getElementById("id_business_version").innerHTML="Business version: 2018
 			0,
 			2* Math.PI);
 		 context.stroke();	
-		 last_pos.x=e.changedTouches[i].pageX;
-		last_pos.y=e.changedTouches[i].pageY;
+		 pozitii[j].x=e.changedTouches[i].pageX;
+		 pozitii[j].y=e.changedTouches[i].pageY;
 	   }
 	   
 	   }
